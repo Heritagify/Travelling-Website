7db4851e15124521b62e0004f60ba17ff8a5305d
mona <heritageolaiy@gmail.com>
2024-01-07 01:50:25 +0100

    work work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 // import logoImage from './assets/Group.svg';
 // import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import Map from './Map.jsx'
-import Login from './Login.jsx';
+import Home from './Home.jsx'
+// import Login from './Login.jsx';
 
 function App() {
   return (
     <>
   {/* <Map/> */}
-  <Login/>
+  {/* <Login/> */}
+  <Home/>
     </>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,15 @@
+import { IoAirplane} from "react-icons/io5";
 
 const Home = () => {
-    return
+  return (
+    <>
+      <div className="bg-cover h-[450px] bg-center" style={{ backgroundImage: 'url("/src/assets/lp1.png")' }}>
+      <h1>HELLOOOO</h1>
+      <IoAirplane className="text-white size-2"/>
+
+    </div>
+    </>
+  )
 }
 
-
-
-
-
-export default Home 
+export default Home
