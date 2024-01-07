adf879d039fa873022988bf1e5d1ae902e3bedf8
mona <heritageolaiy@gmail.com>
2024-01-08 00:21:16 +0100

    started working on the landing page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,32 @@
-import { IoAirplane} from "react-icons/io5";
 import logo from './assets/Group.svg'
+import { IoAirplane} from "react-icons/io5";
+import { IoBed } from "react-icons/io5";
+
 
 const Home = () => {
   return (
-    <>
-      <div className="bg-cover h-[450px] bg-center" style={{ backgroundImage: 'url("/src/assets/lp1.png")' }}>
-      <nav className="flex text-white justify">
-          <h1 className="flex">
-              <IoAirplane className="text-white text-2xl"/>Find Flight
-          </h1>
-          <h1 className="flex">
-              <IoAirplane className="text-white text-2xl"/>Find Stays
-          </h1>
-    <img src="{logo}"/>
+    <div className="relative mx-5 mt-5 ">
+      <div className="rounded-3xl p-3 pt-1 bg-cover h-[450px] bg-center bg-gray-900" style={{ backgroundImage: 'url("/src/assets/lp1.png")' }}>
+      <div className="rounded-3xl absolute inset-0 bg-black bg-opacity-30"></div>
+
+      <nav className="relative z-10 flex text-white justify-between p-2 items-center">
+          <div className="flex space-x-5">
+            <h1 className="flex space-x-3">
+                <IoAirplane className="text-white text-2xl"/>Find Flight
+            </h1>
+            <h1 className="flex space-x-4">
+                <IoBed className="text-white text-2xl"/>Find Stays
+            </h1>
+          </div>
+           <img src={logo} className=""/>
+           <div className="flex font-bold space-x-5 items-center">
+             <p className="">LOGIN</p>
+             <button className="text-black bg-white rounded-md p-2">Sign Up</button>
+           </div>
       </nav>
 
     </div>
-    </>
+    </div>
   )
 }
 
