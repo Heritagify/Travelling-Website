93d73f74cbd4822ddd8e25f9108ae50cbb1b28ed
mona <heritageolaiy@gmail.com>
2024-01-07 02:08:56 +0100

    n bsjknsd

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,19 @@
 import { IoAirplane} from "react-icons/io5";
+import logo from './assets/Group.svg'
 
 const Home = () => {
   return (
     <>
       <div className="bg-cover h-[450px] bg-center" style={{ backgroundImage: 'url("/src/assets/lp1.png")' }}>
-      <h1>HELLOOOO</h1>
-      <IoAirplane className="text-white size-2"/>
+      <nav className="flex text-white justify">
+          <h1 className="flex">
+              <IoAirplane className="text-white text-2xl"/>Find Flight
+          </h1>
+          <h1 className="flex">
+              <IoAirplane className="text-white text-2xl"/>Find Stays
+          </h1>
+    <img src="{logo}"/>
+      </nav>
 
     </div>
     </>
