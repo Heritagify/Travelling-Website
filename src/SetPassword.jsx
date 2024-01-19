b9a0ae2f3ed300f193027d09d7fe0f530e09413d
mona <heritageolaiy@gmail.com>
2024-01-19 18:43:24 +0100

    did the set password form

--- src/SetPassword.jsx
+++ src/SetPassword.jsx
@@ -1,10 +1,73 @@
+import { useState } from 'react';
+import { IoChevronBack } from 'react-icons/io5';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import logo from './assets/footLogo.png';
+import myImage from './assets/login1.jpg';
 
 const SetPassword = () => {
-  return (
-    <div>
-      djsiluidjkshsdkjhdfjksdfjk
-    </div>
-  )
-}
+  const [showPassword1, setShowPassword1] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
 
-export default SetPassword
+  const togglePasswordVisibility1 = () => {
+    setShowPassword1(!showPassword1);
+  };
+
+  const togglePasswordVisibility2 = () => {
+    setShowPassword2(!showPassword2);
+  };
+
+  return (
+    <div className="block md:flex bg-gray-200 min-h-full">
+      <div className="md:w-1/2 md:p-14">
+        <img src={logo} className="w-44 md:w-28 mx-8" alt="logo" />
+        <div className="pt-12 mx-3 md:mx-8 text-blackGreen">
+          <Link to="/login" className="flex gap-1 items-center cursor-pointer">
+            <IoChevronBack /> Back to login
+          </Link>
+          <h1 className="text-4xl my-2 tracking-wide font-semibold font-monts">Set a password</h1>
+          <p className="w-96 text-xs font-monts py-2 font-medium">Your previous password has been reset. Please set a new password for your account.</p>
+
+          <fieldset className="flex justify-between items-center border-2 border-green-300 py-1 w-full mt-4 my-5 rounded-md">
+            <legend className="ml-3 text-sm">Create Password</legend>
+            <input
+              type={showPassword1 ? 'text' : 'password'}
+              className="w-full p-1 outline-none px-2 bg-transparent"
+              placeholder="7789BM6X@@H&$K_"
+            />
+            {showPassword1 ? (
+              <FaEyeSlash className="mx-3 cursor-pointer" onClick={togglePasswordVisibility1} />
+            ) : (
+              <FaEye className="mx-3 cursor-pointer" onClick={togglePasswordVisibility1} />
+            )}
+          </fieldset>
+
+          <fieldset className="flex justify-between items-center border-2 border-green-300 py-1 w-full mt-4 my-5 rounded-md">
+            <legend className="ml-3 text-sm">Re-enter Password</legend>
+            <input
+              type={showPassword2 ? 'text' : 'password'}
+              className="w-full p-1 outline-none px-2 bg-transparent"
+              placeholder="1778BM6X!!@H&$K_"
+            />
+            {showPassword2 ? (
+              <FaEyeSlash className="mx-3 cursor-pointer" onClick={togglePasswordVisibility2} />
+            ) : (
+              <FaEye className="mx-3 cursor-pointer" onClick={togglePasswordVisibility2} />
+            )}
+          </fieldset>
+
+          <button className="w-full py-2 rounded-md font-semibold hover:text-white bg-gradient-to-r from-blue-300 to-green-400">
+            Set password
+          </button>
+        </div>
+      </div>
+      <div className="w-1/2 p-5 hidden md:block">
+        <div className="items-center flex justify-center">
+          <img src={myImage} alt="resort" className="rounded-3xl object-cover w-2/3" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SetPassword;
