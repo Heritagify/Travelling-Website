f98a2ce63b93dcc4b2bb2b9ef72afe1a11bc5e4e
mona <heritageolaiy@gmail.com>
2024-01-21 17:09:27 +0100

    working on some responsiveness

--- src/SetPassword.jsx
+++ src/SetPassword.jsx
@@ -18,17 +18,21 @@ const SetPassword = () => {
   };
 
   return (
-    <div className="block md:flex bg-gray-200 min-h-full">
-      <div className="md:w-1/2 md:p-14">
-        <img src={logo} className="w-44 md:w-28 mx-8" alt="logo" />
-        <div className="pt-12 mx-3 md:mx-8 text-blackGreen">
+    <div className="block md:flex bg-gray-200 min-h-screen">
+      <div className="md:w-1/2 md:p-8 lg:p-14">
+        <img src={logo} className="w-32 md:w-28 mx-auto md:mx-0" alt="logo" />
+        <div className="pt-8 mx-3 md:mx-0 md:ml-8 text-blackGreen">
           <Link to="/login" className="flex gap-1 items-center cursor-pointer">
             <IoChevronBack /> Back to login
           </Link>
-          <h1 className="text-4xl my-2 tracking-wide font-semibold font-monts">Set a password</h1>
-          <p className="w-96 text-xs font-monts py-2 font-medium">Your previous password has been reset. Please set a new password for your account.</p>
+          <h1 className="text-2xl md:text-3xl lg:text-4xl my-2 tracking-wide font-semibold font-monts">
+            Set a password
+          </h1>
+          <p className="w-full md:w-96 text-xs md:text-sm font-monts py-2 font-medium">
+            Your previous password has been reset. Please set a new password for your account.
+          </p>
 
-          <fieldset className="flex justify-between items-center border-2 border-green-300 py-1 w-full mt-4 my-5 rounded-md">
+          <fieldset className="relative flex justify-between items-center border-2 border-green-300 py-1 w-full md:w-96 mt-4 my-5 rounded-md">
             <legend className="ml-3 text-sm">Create Password</legend>
             <input
               type={showPassword1 ? 'text' : 'password'}
@@ -36,13 +40,13 @@ const SetPassword = () => {
               placeholder="7789BM6X@@H&$K_"
             />
             {showPassword1 ? (
-              <FaEyeSlash className="mx-3 cursor-pointer" onClick={togglePasswordVisibility1} />
+              <FaEyeSlash className="absolute top-1/2 -mt-3 right-3 cursor-pointer" onClick={togglePasswordVisibility1} />
             ) : (
-              <FaEye className="mx-3 cursor-pointer" onClick={togglePasswordVisibility1} />
+              <FaEye className="absolute top-1/2 -mt-3 right-3 cursor-pointer" onClick={togglePasswordVisibility1} />
             )}
           </fieldset>
 
-          <fieldset className="flex justify-between items-center border-2 border-green-300 py-1 w-full mt-4 my-5 rounded-md">
+          <fieldset className="relative flex justify-between items-center border-2 border-green-300 py-1 w-full md:w-96 mt-4 my-5 rounded-md">
             <legend className="ml-3 text-sm">Re-enter Password</legend>
             <input
               type={showPassword2 ? 'text' : 'password'}
@@ -50,9 +54,9 @@ const SetPassword = () => {
               placeholder="1778BM6X!!@H&$K_"
             />
             {showPassword2 ? (
-              <FaEyeSlash className="mx-3 cursor-pointer" onClick={togglePasswordVisibility2} />
+              <FaEyeSlash className="absolute top-1/2 -mt-3 right-3 cursor-pointer" onClick={togglePasswordVisibility2} />
             ) : (
-              <FaEye className="mx-3 cursor-pointer" onClick={togglePasswordVisibility2} />
+              <FaEye className="absolute top-1/2 -mt-3 right-3 cursor-pointer" onClick={togglePasswordVisibility2} />
             )}
           </fieldset>
 
@@ -61,9 +65,9 @@ const SetPassword = () => {
           </button>
         </div>
       </div>
-      <div className="w-1/2 p-5 hidden md:block">
+      <div className="w-full md:w-1/2 p-5">
         <div className="items-center flex justify-center">
-          <img src={myImage} alt="resort" className="rounded-3xl object-cover w-2/3" />
+          <img src={myImage} alt="resort" className="rounded-3xl object-cover w-full h-64 md:h-full md:w-2/3" />
         </div>
       </div>
     </div>
