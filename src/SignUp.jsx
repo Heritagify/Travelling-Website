c25c15227623d1d94d5aff1ad2323d43182bd614
mona <heritageolaiy@gmail.com>
2024-01-24 19:39:02 +0100

    i am now done with the signup page for now

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -59,99 +59,164 @@ const SignUp = () => {
         />
       </div>
 
-      <div className={`w-2/3 p-6 px-10 text-blackGreen ${fadeIn ? 'opacity-100' : 'opacity-0'}`}>
-        <img src={logo} className='w-28 mb-8' alt="Logo" />
+      <div className={`w-2/3 p-3 px-10 text-blackGreen ${fadeIn ? 'opacity-100' : 'opacity-0'}`}>
+        <img src={logo} className='w-28 mb-6' alt="Logo" />
 
         <div className="">
           <div>
             <h1 className='text-2xl md:text-3xl tracking-wide font-semibold font-monts'>Sign Up</h1>
-            <p className='text-xs text-gray-500 font-monts pb-6 font-medium'>Let’s get you all set up so you can access your personal account.</p>
+            <p className='text-xs text-gray-500 font-monts pb-3 font-medium'>Let’s get you all set up so you can access your personal account.</p>
           </div>
 
-          <form className="space-y-1 onSubmit={formik.handleSubmit}">
+          <form className="space-y-3 onSubmit={formik.handleSubmit}">
             <div className="flex gap-4">
 
-              <fieldset className="w-1/2 p-1 border border-blue-400 rounded-xl px-2">
-                <legend className="text-xs">First Name</legend>
-                <input id="firstName"
-                  className={`w-full bg-transparent text-sm text-gray-800 outline-none ${formik.touched.firstName && formik.errors.firstName ? 'border-red-500' : ''}`}
-                  placeholder="Heritage"
-                  {...formik.getFieldProps('firstName')}
-                />
-                {formik.touched.firstName && formik.errors.firstName && <p className="text-xs text-red-500">{formik.errors.firstName}</p>}
-              </fieldset>
-
-              <fieldset className="w-1/2 border border-blue-400 rounded-xl p-1 px-2">
-                <legend className="text-xs">Last Name</legend>
-                <input id="lastName"
-                  className={`w-full bg-transparent text-sm text-gray-800 outline-none ${formik.touched.lastName && formik.errors.lastName ? 'border-red-500' : ''}`}
-                  placeholder="Harrison"
-                  {...formik.getFieldProps('lastName')}
-                />
-                {formik.touched.lastName && formik.errors.lastName && <p className="text-xs text-red-500">{formik.errors.lastName}</p>}
-              </fieldset>
-            </div>
-
-            <div className="flex gap-4">
-              <fieldset className="w-1/2 border border-blue-500 rounded-xl p-1 px-2">
-                <legend className="text-xs mx-2">Email</legend>
-                <input id="email"
-                      type="email"
-                  className={`w-full bg-transparent text-sm text-gray-800 outline-none ${formik.touched.lastName && formik.errors.lastName ? 'border-red-500' : ''}`}
-                      placeholder="heritageolaiya@gmail.com"
-                      {...formik.getFieldProps('email')}
-                />
-                {formik.touched.email && formik.errors.email && <p className="text-xs text-red-500">{formik.errors.email}</p>}
-              </fieldset>
-
-              <fieldset className="w-1/2 border border-blue-400 rounded-xl px-2">
-                <legend className="text-xs">Phone Number</legend>
-                <input id="phoneNumber"
-                      type='tel'
-                      inputMode="numeric"
-                      pattern="[0-9]*"
-                      className={`w-full bg-transparent text-sm text-gray-800 outline-none ${formik.touched.phoneNumber && formik.errors.phoneNumber ? 'border-red-500' : ''}`}
-                      placeholder="(+234) -8100000336"
-                  {...formik.getFieldProps('phoneNumber')}
-                />
-                {formik.touched.phoneNumber && formik.errors.phoneNumber && <p className="text-xs text-red-500">{formik.errors.phoneNumber}</p>}
-              </fieldset>
-            </div>
-
-            <fieldset className="flex justify-between border border-blue-400 rounded-xl p-1 px-2">
-              <legend className="text-xs">Password</legend>
-              <input
-                  id="password"
-                  type={formik.values.showPassword1 ? 'text' : 'password'}
-                  className={`w-full bg-transparent text-sm text-gray-800 outline-none ${formik.touched.password && formik.errors.password ? 'border-red-500' : ''}`}
-                  placeholder="**************"
-                  required
-                  {...formik.getFieldProps('password')}
-              />
-                  {formik.values.showPassword1 ? (
-                  <FaEyeSlash className="cursor-pointer" onClick={togglePasswordVisibility1} />
-                ) : (
-                  <FaEye className="cursor-pointer" onClick={togglePasswordVisibility1} />
+            <div className="w-1/2">
+              <div>
+                <fieldset className="p-1 border border-blue-400 rounded-xl">
+                  <legend className="text-xs">First Name</legend>
+                  <input
+                    id="firstName"
+                    className={`w-full bg-transparent text-sm text-gray-800 outline-none ${
+                      formik.touched.firstName && formik.errors.firstName
+                        ? 'border-red-500'
+                        : ''
+                    }`}
+                    placeholder="Heritage"
+                    {...formik.getFieldProps('firstName')}
+                  />
+                </fieldset>
+                {formik.touched.firstName && formik.errors.firstName && (
+                  <p className="text-xs text-red-500">{formik.errors.firstName}</p>
                 )}
-                {formik.touched.password && formik.errors.password && <p className="text-xs text-red-500">{formik.errors.password}</p>}
-            </fieldset>
+              </div>
+            </div>
 
-            <fieldset className="flex justify-between border border-blue-400 rounded-xl p-1 px-2">
-              <legend className="text-xs">Confirm Password</legend>
-              <input
-                id="confirmPassword"
-                type={formik.values.showPassword2 ? 'text' : 'password'}
-                className={`w-full bg-transparent text-sm text-gray-800 outline-none ${formik.touched.confirmPassword && formik.errors.confirmPassword ? 'border-red-500' : ''}`}
-                placeholder="**************"
-                {...formik.getFieldProps('confirmPassword')}
-              />
-                  {formik.values.showPassword2 ? (
-                <FaEyeSlash className="cursor-pointer" onClick={togglePasswordVisibility2} />
-              ) : (
-                <FaEye className="cursor-pointer" onClick={togglePasswordVisibility2} />
-              )}
-              {formik.touched.confirmPassword && formik.errors.confirmPassword && <p className="text-xs text-red-500">{formik.errors.confirmPassword}</p>}
-            </fieldset>
+                <div className="w-1/2">
+                  <div>
+                    <fieldset className="border border-blue-400 rounded-xl p-1">
+                      <legend className="text-xs mx-2">Last Name</legend>
+                      <input
+                        id="lastName"
+                        className={`w-full bg-transparent text-sm text-gray-800 outline-none ${
+                          formik.touched.lastName && formik.errors.lastName
+                            ? 'border-red-500'
+                            : ''
+                        }`}
+                        placeholder="Harrison"
+                        {...formik.getFieldProps('lastName')}
+                      />
+                    </fieldset>
+                    {formik.touched.lastName && formik.errors.lastName && (
+                      <p className="text-xs text-red-500">{formik.errors.lastName}</p>
+                    )}
+                  </div>
+              </div>
+          </div>
+
+
+
+          <div>
+  <div className="flex gap-4">
+    <div className="w-1/2">
+      <div>
+        <fieldset className="border border-blue-500 rounded-xl p-1 px-2">
+          <legend className="text-xs mx-2">Email</legend>
+          <input
+            id="email"
+            type="email"
+            className={`w-full bg-transparent text-sm text-gray-800 outline-none ${
+              formik.touched.email && formik.errors.email
+                ? 'border-red-500'
+                : ''
+            }`}
+            placeholder="heritageolaiya@gmail.com"
+            {...formik.getFieldProps('email')}
+          />
+        </fieldset>
+        {formik.touched.email && formik.errors.email && (
+          <p className="text-xs text-red-500">{formik.errors.email}</p>
+        )}
+      </div>
+    </div>
+
+    <div className="w-1/2">
+      <div>
+        <fieldset className="border border-blue-400 rounded-xl p-1 px-2">
+          <legend className="text-xs">Phone Number</legend>
+          <input
+            id="phoneNumber"
+            type="tel"
+            inputMode="numeric"
+            pattern="[0-9]*"
+            className={`w-full bg-transparent text-sm text-gray-800 outline-none ${
+              formik.touched.phoneNumber && formik.errors.phoneNumber
+                ? 'border-red-500'
+                : ''
+            }`}
+            placeholder="(+234) -8100000336"
+            {...formik.getFieldProps('phoneNumber')}
+          />
+        </fieldset>
+        {formik.touched.phoneNumber && formik.errors.phoneNumber && (
+          <p className="text-xs text-red-500">{formik.errors.phoneNumber}</p>
+        )}
+      </div>
+    </div>
+  </div>
+
+  <div className=" my-2">
+    <fieldset className="flex justify-between border border-blue-400 rounded-xl p-1 px-2 ">
+      <legend className="text-xs">Password</legend>
+      <input
+        id="password"
+        type={formik.values.showPassword1 ? 'text' : 'password'}
+        className={`w-full bg-transparent text-sm text-gray-800 outline-none ${
+          formik.touched.password && formik.errors.password
+            ? 'border-red-500'
+            : ''
+        }`}
+        placeholder="**************"
+        required
+        {...formik.getFieldProps('password')}
+      />
+      {formik.values.showPassword1 ? (
+        <FaEyeSlash className="cursor-pointer" onClick={togglePasswordVisibility1} />
+      ) : (
+        <FaEye className="cursor-pointer" onClick={togglePasswordVisibility1} />
+      )}
+    </fieldset>
+    {formik.touched.password && formik.errors.password && (
+      <p className="text-xs text-red-500">{formik.errors.password}</p>
+    )}
+  </div>
+
+  <div className="2">
+    <fieldset className="flex justify-between border border-blue-400 rounded-xl p-1 px-">
+      <legend className="text-xs">Confirm Password</legend>
+      <input
+        id="confirmPassword"
+        type={formik.values.showPassword2 ? 'text' : 'password'}
+        className={`w-full bg-transparent text-sm text-gray-800 outline-none ${
+          formik.touched.confirmPassword && formik.errors.confirmPassword
+            ? 'border-red-500'
+            : ''
+        }`}
+        placeholder="**************"
+        {...formik.getFieldProps('confirmPassword')}
+      />
+      {formik.values.showPassword2 ? (
+        <FaEyeSlash className="cursor-pointer" onClick={togglePasswordVisibility2} />
+      ) : (
+        <FaEye className="cursor-pointer" onClick={togglePasswordVisibility2} />
+      )}
+    </fieldset>
+    {formik.touched.confirmPassword && formik.errors.confirmPassword && (
+      <p className="text-xs text-red-500">{formik.errors.confirmPassword}</p>
+    )}
+  </div>
+</div>
+
           </form>
 
           <div className='flex gap-2 mt-4'>
@@ -161,10 +226,10 @@ const SignUp = () => {
                 className={` bg-transparent text-sm text-gray-800 outline-none ${formik.touched.agreeCheckbox && formik.errors.agreeCheckbox ? 'border-red-500' : ''}`}
                 {...formik.getFieldProps('agreeCheckbox')}
             />
-                {formik.touched.agreeCheckbox && formik.errors.agreeCheckbox && <p className="text-xs text-red-500">{formik.errors.agreeCheckbox}</p>}
             <label htmlFor="agreeCheckbox" className='text-monts text-sm'>
               I agree to all the <span className='text-red-600'>Terms</span> and <span className='text-red-600'>Privacy Policies</span>
             </label>
+                {formik.touched.agreeCheckbox && formik.errors.agreeCheckbox && <p className="text-xs text-red-500">{formik.errors.agreeCheckbox}</p>}
           </div>
 
           <div className='space-y-2 mt-4'>
@@ -175,9 +240,9 @@ const SignUp = () => {
               <Link to="/login" className='text-red-500 px-1'>Login</Link>
             </p>
             <div className="flex items-center py-3">
-              <div className="w-52 border-t border-blue-500"></div>
+              <div className="w-52 border-t border-blue-300"></div>
               <div className="mx-1 text-sm text-gray-500">or login with</div>
-              <div className="w-52 border-t border-blue-500"></div>
+              <div className="w-52 border-t border-blue-300"></div>
             </div>
 
             {/* -----------OTHER LOGIN OPTIONS----------- */}
