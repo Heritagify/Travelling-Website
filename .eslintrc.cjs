a178a1b26a4625c2fca2875d1dfd6d1ca9ca5609
mona <heritageolaiy@gmail.com>
2024-02-14 20:20:35 +0100

    hmmmm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,11 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, node: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:node/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
