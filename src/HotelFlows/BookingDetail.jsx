42a44877861d0e17021f2eeae6fcbde8c88c1d4e
mona <heritageolaiy@gmail.com>
2024-03-01 19:14:30 +0100

    more design

--- src/HotelFlows/BookingDetail.jsx
+++ src/HotelFlows/BookingDetail.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import AccountsNav from '../components/Headers/AccountsNav';
 import profilePicture from "../assets/user.png";
+import cvk from "../assets/cvk.png"
+import { MdOutlineChevronRight } from "react-icons/md";
+import { HiOutlineBuildingOffice2 } from "react-icons/hi2"; <HiOutlineBuildingOffice2 />
+// import { HiArrowLongLeft, HiArrowLongRight } from "react-icons/hi2"; <HiArrowLongLeft /> <HiArrowLongRight />
+
+
+
+
+
 
 
 const BookingDetail = () => {
@@ -12,9 +21,38 @@ const BookingDetail = () => {
 
   return (
     <div>
-      {/* Include AccountsNav and pass the user prop */}
-      <AccountsNav user={user} />
-      {/* Other content of BookingDetail component */}
+      {/* <AccountsNav user={user} /> */}
+      <p className='flex items-center space-x-1'>
+        <span className='text-red-400'>Turkey</span> <MdOutlineChevronRight />
+        <span className='text-red-400'>Istanbul </span> <MdOutlineChevronRight />
+        CVK Park Bosphorus Hotel Istanbul
+      </p>
+
+      <div className=' flex border-4 border-red-500'>
+      {/* ---------1-------- */}
+        <div className='border-4 border-green-500 text-blackGreen rounded-sm'>
+          <div className='flex justify-between'>
+            <p>Superior room - 1 double bed or 2 twin beds</p>
+            <p className='text-red-500'>$240 <span>/night</span></p>
+          </div>
+          <div className='flex border-2 border-lightMint'>
+            <img
+              src={cvk}
+              alt='Cvk park'
+            />
+            <div className=''>
+              <h1>CVK Park Bosphorus Hotel Istanbul</h1>
+              <p> Gummusuyu Mah.Inonu Cad. No8, Istanbul 34437</p>
+            </div>
+            <div></div>
+          </div>
+
+        </div>
+
+        {/* ---------------2----------- */}
+        <div className=''></div>
+
+      </div>
     </div>
   );
 };
