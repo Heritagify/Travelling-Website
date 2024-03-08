cb0f845e440c076632c59d74c0b7f13613133993
mona <heritageolaiy@gmail.com>
2024-03-08 17:43:29 +0100

    working on some responsiveness

--- src/components/Headers/AccountsNav.jsx
+++ src/components/Headers/AccountsNav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import logo from "../../assets/footLogo.png";
-import { FaHeart, FaBars } from 'react-icons/fa';
+import { FaHeart, FaBars, FaTimes } from 'react-icons/fa';
 import { IoAirplane, IoBed } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 
@@ -14,11 +14,11 @@ const AccountsNav = ({ user }) => {
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-10">
-      <div className="container mx-auto px-4 md:px-8">
+      <div className="container mx-auto px-4 md:px-0 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <div className="flex items-center space-x-5">
             <button className="md:hidden" onClick={toggleMenu}>
-              <FaBars className="h-6 w-6 fill-current text-blackGreen" />
+              {isMenuOpen ? <FaTimes className="h-6 w-6 fill-current text-blackGreen" /> : <FaBars className="h-6 w-6 fill-current text-blackGreen" />}
             </button>
             <Link to="/" className={`hidden md:flex items-center text-blackGreen text-lg ${isMenuOpen ? 'hidden' : ''}`}>
               <IoAirplane className="mr-1 text-2xl" />
@@ -34,22 +34,24 @@ const AccountsNav = ({ user }) => {
             <img src={logo} className="w-24 md:w-28" alt="Logo" />
           </Link>
 
-          <div className="hidden md:flex items-center space-x-1">
-            <FaHeart className="text-xl text-blackGreen" />
-            <span className="text-sm md:text-base">Favourites</span>
-            <span className="text-2xl">|</span>
-          </div>
-          
-          {user && (
-            <div className="flex items-center gap-1">
-              <img
-                src={user.profilePicture}
-                alt={`Profile of ${user.name}`}
-                className="w-10 h-10 rounded-full mr-2"
-              />
-              <span className="text-blackGreen font-semibold">{user.name}</span>
+          <div className='flex gap-6'>
+            <div className="hidden md:flex items-center space-x-1">
+              <FaHeart className="text-xl text-blackGreen" />
+              <span className="text-sm md:text-base">Favourites</span>
+              <span className="text-lg">|</span>
             </div>
-          )}
+  
+            {user && (
+              <div className="flex items-center gap-1">
+                <img
+                  src={user.profilePicture}
+                  alt={`Profile of ${user.name}`}
+                  className="w-10 h-10 rounded-full mr-2"
+                />
+                <span className="text-blackGreen font-semibold hidden md:flex">{user.name}</span>
+              </div>
+            )}
+          </div>
         </div>
         {/* Collapsible menu */}
         {isMenuOpen && (
