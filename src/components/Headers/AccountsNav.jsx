649148ddc3cf623393433b88b8406042d4c9c787
mona <heritageolaiy@gmail.com>
2024-03-22 16:07:28 +0100

    did some responsiveness

--- src/components/Headers/AccountsNav.jsx
+++ src/components/Headers/AccountsNav.jsx
@@ -57,19 +57,19 @@ const AccountsNav = ({ user, logout }) => {
                   <span className="text-blackGreen font-semibold hidden md:flex">{user.name}</span>
                 </button>
                 {isMenuOpen && (
-                  <div className="absolute right-0 top-full mt-2 w-48 bg-lightMint shadow-lg rounded-lg">
+                  <div className="absolute right-0 md:-right-3 lg:right-0 top-full mt-2 w-52 md:w-64 lg:w-48 bg-mintGreen shadow shadow-green-700 rounded-lg">
                     <div className="flex items-center justify-between p-4">
                       <div className='flex items-center'>
-                        <img src={user.profilePicture} alt={`Profile of ${user.name}`} className="w-10 h-10 rounded-full mr-2" />
-                        <span className="text-sm font-semibold">{user.name}</span>
+                        <img src={user.profilePicture} alt={`Profile of ${user.name}`} className="w-14 lg:w-10 h-14 lg:h-10 rounded-full mr-2" />
+                        <span className="lg:text-sm font-semibold">{user.name}</span>
                       </div>
-                      <span className="w-2 h-2 rounded-full bg-green-500"></span> {/* Online status indicator */}
+                      <span className="w-3 lg:w-2 h-3 lg:h-2 rounded-full bg-green-500"></span> {/* Online status indicator */}
                     </div>
-                    <Link to="/account" className="flex items-center gap-1 py-2 px-3 text-xs font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9"><FaUser className='text-sm'/>My Account</Link>
-                    <Link to="/payments" className="flex items-center gap-1 py-2 px-3 text-xs font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9"><MdPayments className='text-sm'/>Payments</Link>
-                    <Link to="/settings" className="flex items-center gap-1 py-2 px-3 text-xs font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9"><RiUserSettingsFill className='text-sm'/>Settings</Link>
-                    <Link to="/support" className="flex items-center gap-1 py-2 px-3 text-xs font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9"><FcCustomerSupport className='text-sm'/>Support</Link>
-                    <button className="flex items-center gap-1 w-full text-left py-2 px-3 text-xs font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9 focus:outline-none" onClick={logout}><RiLogoutCircleFill className='text-sm'/>Logout</button>
+                    <Link to="/account" className="flex items-center gap-1 py-2 px-3 text-sm md:text-lg lg:text-xs font-medium md:font-semibold lg:font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9"><FaUser className='md:text-xl lg:text-sm'/>My Account</Link>
+                    <Link to="/payments" className="flex items-center gap-1 py-2 px-3 text-sm md:text-lg lg:text-xs font-medium md:font-semibold lg:font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9"><MdPayments className='md:text-xl lg:text-sm'/>Payments</Link>
+                    <Link to="/settings" className="flex items-center gap-1 py-2 px-3 text-sm md:text-lg lg:text-xs font-medium md:font-semibold lg:font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9"><RiUserSettingsFill className='md:text-xl lg:text-sm'/>Settings</Link>
+                    <Link to="/support" className="flex items-center gap-1 py-2 px-3 text-sm md:text-lg lg:text-xs font-medium md:font-semibold lg:font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9"><FcCustomerSupport className='md:text-xl lg:text-sm'/>Support</Link>
+                    <button className="flex items-center gap-1 w-full text-left py-2 px-3 text-sm md:text-lg lg:text-xs font-medium md:font-semibold lg:font-medium font-monts hover:bg-blackGreen hover:text-mintGreen hover:px-9 focus:outline-none" onClick={logout}><RiLogoutCircleFill className='md:text-xl lg:text-sm'/>Logout</button>
                   </div>
                 )}
               </div>
