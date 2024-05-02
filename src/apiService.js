71f745dc3b888e6d9e43c89d8207d47e282bd868
mona <heritageolaiy@gmail.com>
2024-05-02 20:07:00 +0100

    changes

--- src/apiService.js
+++ src/apiService.js
@@ -1,16 +1,16 @@
-// apiService.js
+// // apiService.js
 
-import Axios from 'axios';
+// import Axios from 'axios';
 
-const addUserToDatabase = async (userData) => {
-  try {
-    const response = await Axios.post('http://localhost:4000/api/users', userData);
-    console.log(response.data); // Log the response from the backend
-    return response.data; // Optionally, you can return the response data
-  } catch (error) {
-    console.error('Error adding user to database:', error);
-    throw error; // Throw the error so it can be handled in the component
-  }
-};
+// const addUserToDatabase = async (userData) => {
+//   try {
+//     const response = await Axios.post('http://localhost:4000/api/users', userData);
+//     console.log(response.data); // Log the response from the backend
+//     return response.data; // Optionally, you can return the response data
+//   } catch (error) {
+//     console.error('Error adding user to database:', error);
+//     throw error; // Throw the error so it can be handled in the component
+//   }
+// };
 
-export { addUserToDatabase };
+// export { addUserToDatabase };
